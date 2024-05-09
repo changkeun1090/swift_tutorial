c242cc3e88fbeb5ab4f99dd8650cb0ec31fb41f9
changkeun <changkeun1090@gmail.com>
2024-05-09 09:03:25 +0900

    section.4 델리게이트패턴 마무리

--- allen_project/section_4/TextFieldProject/TextFieldProject/ViewController.swift
+++ allen_project/section_4/TextFieldProject/TextFieldProject/ViewController.swift
@@ -1,7 +1,6 @@
 import UIKit
 
 class ViewController: UIViewController, UITextFieldDelegate {
-
       
     @IBOutlet weak var textField: UITextField!
         
@@ -19,15 +18,25 @@ class ViewController: UIViewController, UITextFieldDelegate {
         textField.borderStyle = .roundedRect
         textField.clearButtonMode = .always
         textField.returnKeyType = .go
+        
+        // textfield를 화면을 열자마자 가장 먼저 반응하는 객체로 만들어준다.
+//        textField.becomeFirstResponder()
     }
     
+    // 화면 터치시 키보드 내림
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+       }
+    
     @IBAction func doneButtonTapped(_ sender: UIButton) {
+        // 키보드를 내림
+        textField.resignFirstResponder()
     }
     
     // textfield 입력 시작될 때 호출되어 textfield 편집 허락여부 결정
     func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
         return true
-    } 
+    }
     
     // textfield 입력을 시작될 때 호출
     func textFieldDidBeginEditing(_ textField: UITextField) {
