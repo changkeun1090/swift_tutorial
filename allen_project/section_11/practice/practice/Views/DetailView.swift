7b840f127916fee1633a6f640f111adabe2705a3
changkeun <changkeun1090@gmail.com>
2024-06-01 21:11:01 +0900

    section.11 델리게이트패턴 연습

--- allen_project/section_11/practice/practice/Views/DetailView.swift
+++ allen_project/section_11/practice/practice/Views/DetailView.swift
@@ -1,20 +1,5 @@
-//
-//  DetailView.swift
-//  Practice
-//
-//  Created by ChangKeun Ji on 5/29/24.
-//
-
 import UIKit
 
 class DetailView: UIView {
-
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
-    }
-    */
 
 }
